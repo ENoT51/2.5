c14aeefbe221a8d80421b43e99010380e576e987
ENoT51 <ambarov515@gmail.com>
2024-06-02 16:56:23 +0300

    2.6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String REGEX = "^[a-zA-Z0-9_]*$";
     public static void main(String[] args) {
+        try {
+            checkAuthorization("adminN", "1234", "1234");
+            System.out.println("Проверка пройдена");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Проверка не пройдена");;
+        }
+    }
+    public static void checkAuthorization(String login, String password, String confirmPassword){
+    if (login.length() > 20 ){
+        throw new WrongLoginException();
+        }
+    if (!login.matches(REGEX)){
+        throw new WrongLoginException();
+    }
+    if (password.length() > 20 ){
+        throw new WrongPasswordException();
+    }
+    if (!password.matches(REGEX)){
+        throw new WrongPasswordException();
+    }
+    if (!password.equals(confirmPassword)){
+        throw new WrongPasswordException();
+    }
+
 
     }
 }
